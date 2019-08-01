4e2bf51c77e500720694bca137fad473e310ce87
Simon <simon.chamorro@usherbrooke.ca>
2019-08-01 16:28:05 -0400

    plot

--- scripts/04-pull-ntrip-read-gps.py
+++ scripts/04-pull-ntrip-read-gps.py
@@ -1,7 +1,9 @@
 from multiprocessing import Queue, Process
 
 import cv2
+import os
 import serial
+import time
 import pynmea2
 from pynmea2 import GGA
 
@@ -10,8 +12,8 @@ from neom8p.gmaps import get_gmap
 USER = "tr+mil01286801"
 PASS = "51118945"
 HOST = "qc.smartnetna.com"
-PORT = 10000
-MOUNT = "/MSM_VIRS"
+PORT = 9950
+MOUNT = "/RTCM3_VRS_IGS"
 
 def gpsreader(q, sock):
     while True:
@@ -41,35 +43,61 @@ def mapdisplayer(q):
         cv2.imshow("live map", map)
         cv2.waitKey(1)
 
-
-socket = serial.Serial(port="/dev/cu.usbserial-14410", baudrate=9600)
+socket = serial.Serial(port="/dev/ttyUSB0", baudrate=9600)
 
 q_preproc = Queue()
 q_display = Queue()
 
 gpsr = Process(target=gpsreader, args=(q_preproc,socket))
 ntrs = Process(target=ntripsender, args=(socket,))
-mapd = Process(target=mapdisplayer, args=(q_display,))
+# mapd = Process(target=mapdisplayer, args=(q_display,))
+
+
+log_num = 0
+while f'log_{log_num}.csv' in os.listdir('./logs'):
+    log_num += 1
+fname  = f'./logs/log_{log_num}.csv'
+
+if os.path.isfile(fname):
+    print('file already exists')
+    raise Exception
+
+f = open(fname, 'w')
+f.write('timestamp lat lon gps_qual num_sats horizontal_dil\n')
 
 gpsr.start()
 ntrs.start()
-mapd.start()
+# mapd.start()
+
+last_timestamp = None
+timestamp = None
+lat = None
+lon = None
+gps_qual = None
+num_sats = None
+horizontal_dil = None
 
 while True:
 
     data = q_preproc.get()
     try:
         data = data.decode()
-        # print(data)
         data_nmea = pynmea2.parse(data)
-
         if hasattr(data_nmea, "gps_qual"):
-            print (f"GPS Qaulity: {data_nmea.gps_qual}, No satellites: {data_nmea.num_sats}, horz. DOP: {data_nmea.horizontal_dil}")
-
-        # print (type(data_nmea), data_nmea)
+            gps_qual = data_nmea.gps_qual
+            num_sats = data_nmea.num_sats
+            horizontal_dil = data_nmea.horizontal_dil
+            print (f"GPS Quality: {data_nmea.gps_qual}, Num satellites: {data_nmea.num_sats}, horz. DOP: {data_nmea.horizontal_dil}")
         if hasattr(data_nmea, 'lat'):
-            print (f"lat, lon: {data_nmea.latitude}, {data_nmea.longitude}")
+            timestamp = data_nmea.timestamp
+            lat = data_nmea.latitude
+            lon = data_nmea.longitude
+            print (f"time : {data_nmea.timestamp}, lat, lon: {data_nmea.latitude}, {data_nmea.longitude}")
             q_display.put((data_nmea.latitude,data_nmea.longitude))
+        if not last_timestamp == timestamp:
+            f.write(f"{timestamp} {lat} {lon} {gps_qual} {num_sats} {horizontal_dil}\n")
+            last_timestamp = timestamp
+
     except UnicodeDecodeError:
         pass # usually only the first message is fucked. Can also happen if wires disconnect
     except  pynmea2.nmea.ParseError:
--- scripts/06-plot-gps-data.py
+++ scripts/06-plot-gps-data.py
@@ -5,7 +5,7 @@ import numpy as np
 from geopy import distance
 import tilemapbase
 
-def read_gps(path):
+def read_gps_raw(path):
     regex = r"\$GNRMC,([0-9\.]+)?,([VA]),(([0-9.]+)([0-9]{2}\.[0-9]+))?,([NS])?,(([0-9.]+)([0-9]{2}\.[0-9]+))?,([EW])?,([0-9.]+)?,([0-9.]+)?,([0-9.]+)?,([0-9.]+)?,([0-9.]+)?,(.*)?"
     time_regex = r"([0-9]+)?:([0-9]+)?:([0-9\.]+)?"
     coords = []
@@ -29,11 +29,27 @@ def read_gps(path):
                     speedkm = round(float(match.group(11)), 2)
                     time = re.match(time_regex, line.split()[0])
                     timestamp = calendar.timegm((1970, 1, 1, int(time.group(1)), int(time.group(2)), float(time.group(3))))
-                    print(f'gps: {latitude}, {longitude} timestamp: {timestamp}')
+                    # print(f'gps: {latitude}, {longitude} timestamp: {timestamp}')
                     coords.append((timestamp, latitude, longitude))
                 else:
                     pass
     return coords
+
+def read_gps(path):
+    coords = []
+    gps_qual = []
+    with open(path, 'r') as fid:
+        while True:
+            line = fid.readline()
+            if not line:
+                break
+            line = line.replace('\n', '').split()
+            if 'timestamp' in line or 'None' in line:
+                continue
+            timestamp = calendar.timegm((1970, 1, 1, int(line[0].split(':')[0]), int(line[0].split(':')[1]), float(line[0].split(':')[2])))
+            coords.append((timestamp, float(line[1]), float(line[2])))
+            gps_qual.append((float(line[3]), float(line[4]), float(line[5])))
+    return coords, gps_qual
 
 def plot_coords(ground_truth, coords, degree_range=0.0001):
     tilemapbase.start_logging()
@@ -41,8 +57,10 @@ def plot_coords(ground_truth, coords, degree_range=0.0001):
     t = tilemapbase.tiles.build_OSM()
 
     degree_range = degree_range
-    extent = tilemapbase.Extent.from_lonlat(ground_truth[1] - degree_range, ground_truth[1] + degree_range,
-             ground_truth[0] - degree_range, ground_truth[0] + degree_range)
+    center_lat = np.mean(np.array([coord[0] for coord in ground_truth]))
+    center_lon = np.mean(np.array([coord[1] for coord in ground_truth]))
+    extent = tilemapbase.Extent.from_lonlat(center_lon - degree_range, center_lon + degree_range,
+             center_lat - degree_range, center_lat + degree_range)
     extent = extent.to_aspect(1.0)
     fig, ax = plt.subplots(figsize=(8, 8), dpi=100)
     ax.xaxis.set_visible(False)
@@ -50,8 +68,10 @@ def plot_coords(ground_truth, coords, degree_range=0.0001):
 
     plotter = tilemapbase.Plotter(extent, t, width=600)
     plotter.plot(ax, t)
-    x, y = tilemapbase.project(ground_truth[1], ground_truth[0])
-    ax.scatter(x,y, marker="*", color="red", linewidth=5)
+
+    # for coord in ground_truth:
+    #     x, y = tilemapbase.project(coord[1], coord[0])
+    #     ax.scatter(x,y, marker="*", color="red", linewidth=5)
     for idx, coord in enumerate(coords):
         x, y = tilemapbase.project(coord[2], coord[1])
         ax.scatter(x,y, marker=".", c=(0, 1 - (1/len(coords))*idx, (1/len(coords))*idx), linewidth=1)
@@ -69,23 +89,26 @@ def plot_error(ground_truth, coords, degree_range=0.0001):
     plt.ylabel('error of avg coords (m)')
     plt.show()
 
+def plot_gps_qual(coords, gps_qual):
+    t = [coord[0] - coords[0][0] for coord in coords]
+    qual = np.array([tup[0] for tup in gps_qual])
+    num_sats = np.array([tup[1] for tup in gps_qual])
+    horizontal_dop = np.array([tup[2] for tup in gps_qual])
+    plt.plot(t, qual, 'r-', label="Quality")
+    # plt.plot(t, num_sats, 'g-', label="Num Sats")
+    plt.plot(t, horizontal_dop, 'b-', label="Horizontal DOP")
+    plt.show()
 
-# coords = read_gps('gps_test1.txt')
-# plot_coords((45.530505, -73.614722), coords)
-# plot_error((45.530505, -73.614722), coords)
 
-# coords = read_gps('gps_test2.txt')
-# plot_coords((45.530693, -73.614824), coords)
-# plot_error((45.530693, -73.614824), coords)
+# coords = read_gps_raw('./logs/raw/gps_test3.txt')
+# plot_coords([(45.531104, -73.613218)], coords)
+# plot_error([(45.531104, -73.613218)], coords)
+# coords = read_gps_raw('./logs/raw/block.txt')
+# plot_coords([(45.531308, -73.614679)], coords, degree_range=0.002)
 
-coords = read_gps('gps_test3.txt')
-plot_coords((45.531104, -73.613218), coords)
-plot_error((45.531104, -73.613218), coords)
-
-coords = read_gps('gps_test4.txt')
-plot_coords((45.530997, -73.613050), coords)
-plot_error((45.530997, -73.613050), coords)
-
-# coords = read_gps('block.txt')
-# plot_coords((45.531308, -73.614679), coords, degree_range=0.002)
-
+coords, gps_qual = read_gps('./logs/street.csv')
+plot_coords([(45.531308, -73.614679)], coords, degree_range=0.002)
+plot_gps_qual(coords, gps_qual)
+# coords, gps_qual = read_gps('./logs/square.csv')
+# plot_coords([(45.529989, -73.613435)], coords, degree_range=0.0002)
+# plot_gps_qual(coords, gps_qual)
